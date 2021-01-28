9e914f580c4c7bf95c1b1ec3420a71a80f474bf6
brody <brodyw@mit.edu>
2021-01-28 12:29:14 -0500

    Moved brain_score_submission back to the repo, fixed cornet_irl packaging

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 requirements = [
     "torch",
@@ -14,6 +14,7 @@ setup(name='cornet_irl',
       version='0.2',
       description='CORnet-S but also trained on surface normals',
       url='http://github.com/bwest25/cornet_irl',
+      packages=find_packages(include=['cornet_irl']),
       author='Brody West',
       author_email='brodyw@mit.edu',
       install_requires=requirements,
